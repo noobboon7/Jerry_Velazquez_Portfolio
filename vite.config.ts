b5e65c763dde495de95c8fe65e484efdff9c48fb
Jerry Velazquez <Jvr572@gmail.com>
2024-12-24 10:41:36 -0500

    added configurations for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Jerry_Velazquez_Portfolio/',
   plugins: [react()],
   server: {
     port: 3000
